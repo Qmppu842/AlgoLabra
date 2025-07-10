e6115c11a7f41dcaa86e2f09eed098bd1910d98c
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-07-10 13:50:02 +0300

    Now the ktlint and detetk should be at least bit happy.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,14 +5,21 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
+
+    id("io.kotest.multiplatform") version "6.0.0.M4"
+
+    id("org.jetbrains.kotlinx.kover") version "0.9.1"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+
+    id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
 }
 
 kotlin {
     jvm("desktop")
-    
+
     sourceSets {
         val desktopMain by getting
-        
+
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
@@ -33,7 +40,6 @@ kotlin {
     }
 }
 
-
 compose.desktop {
     application {
         mainClass = "io.qmpu842.labs.MainKt"
@@ -44,4 +50,72 @@ compose.desktop {
             packageVersion = "1.0.0"
         }
     }
+}
+
+ktlint {
+//    additionalEditorconfigFile.set(file("../.editorconfig"))
+//    experimental = true
+//    ruleSets = listOf("io.nlopez.compose.rules.ktlint.ComposeKtLintRuleSetProvider")
+}
+
+detekt {
+    // Version of detekt that will be used. When unspecified the latest detekt
+    // version found will be used. Override to stay on the same version.
+    toolVersion = "1.23.8"
+
+    // The directories where detekt looks for source files.
+    // Defaults to `files("src/main/java", "src/test/java", "src/main/kotlin", "src/test/kotlin")`.
+    source.setFrom(
+        "src/main/java",
+        "src/main/kotlin",
+        "composeApp/src/desktopMain/kotlin/io/qmpu842/labs",
+        "composeApp/src/desktopTest/kotlin/io/qmpu842/labs",
+        "src/desktopMain/kotlin/io/qmpu842/labs",
+        "src/desktopTest/kotlin/io/qmpu842/labs",
+    )
+
+    // Builds the AST in parallel. Rules are always executed in parallel.
+    // Can lead to speedups in larger projects. `false` by default.
+    parallel = false
+
+    // Define the detekt configuration(s) you want to use.
+    // Defaults to the default detekt configuration.
+//    config.setFrom("detetk.yml")
+    config.setFrom(rootProject.file("detekt.yml"))
+
+    autoCorrect = true
+
+    // Applies the config files on top of detekt's default config file. `false` by default.
+//    buildUponDefaultConfig = false
+    buildUponDefaultConfig = true
+
+    // Turns on all the rules. `false` by default.
+    allRules = false
+
+    // Specifying a baseline file. All findings stored in this file in subsequent runs of detekt.
+//    baseline = file("path/to/baseline.xml")
+
+    // Disables all default detekt rulesets and will only run detekt with custom rules
+    // defined in plugins passed in with `detektPlugins` configuration. `false` by default.
+    disableDefaultRuleSets = false
+
+    // Adds debug output during task execution. `false` by default.
+    debug = false
+
+    // If set to `true` the build does not fail when there are any issues.
+    // Defaults to `false`.
+    ignoreFailures = false
+
+    // Android: Don't create tasks for the specified build types (e.g. "release")
+    ignoredBuildTypes = listOf("release")
+
+    // Android: Don't create tasks for the specified build flavor (e.g. "production")
+    ignoredFlavors = listOf("production")
+
+    // Android: Don't create tasks for the specified build variants (e.g. "productionRelease")
+    ignoredVariants = listOf("productionRelease")
+
+    // Specify the base path for file paths in the formatted reports.
+    // If not set, all file paths reported will be absolute file path.
+//    basePath.set(projectDir)
 }
