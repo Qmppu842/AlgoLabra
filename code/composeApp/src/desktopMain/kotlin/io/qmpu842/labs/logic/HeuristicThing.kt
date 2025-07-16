db7d6e9c7186c9d19d4926ab1e651f66d1f88daa
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-07-16 23:34:11 +0300

    Added TDD structure for heuristic fun.

---
+++ code/composeApp/src/desktopMain/kotlin/io/qmpu842/labs/logic/HeuristicThing.kt
@@ -0,0 +1,13 @@
+package io.qmpu842.labs.logic
+
+object HeuristicThing {
+    fun singleWellHeuristic(
+        column: Int,
+        board: Board,
+        neededForWin: Int = 4,
+    ): Int {
+        println("asdas")
+
+        return 0
+    }
+}
---
+++ code/composeApp/src/desktopTest/kotlin/io/qmpu842/labs/logic/HeuristicThingTest.kt
@@ -0,0 +1,40 @@
+package io.qmpu842.labs.logic
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.ints.shouldBeGreaterThan
+import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
+
+class HeuristicThingTest :
+    FunSpec({
+
+        beforeTest {
+        }
+
+        test("singleWellHeuristic should give higher score to middle than to edges") {
+            var board =
+                Board(
+                    board =
+                        arrayOf(
+                            intArrayOf(0, 0, 0, 0, 0, 0),
+                            intArrayOf(0, 0, 0, 0, 0, 0),
+                            intArrayOf(0, 0, 0, 0, 0, 0),
+                            intArrayOf(0, 0, 0, 0, 0, 0),
+                            intArrayOf(0, 0, 0, 0, 0, 0),
+                            intArrayOf(0, 0, 0, 0, 0, 0),
+                            intArrayOf(0, 0, 0, 0, 0, 0),
+                        ),
+                )
+
+            val rightWell = HeuristicThing.singleWellHeuristic(3, board)
+
+            val middleWell = HeuristicThing.singleWellHeuristic(0, board)
+
+            val leftWell = HeuristicThing.singleWellHeuristic(6, board)
+
+            rightWell shouldBeEqual leftWell
+            middleWell shouldBeGreaterThan rightWell
+            middleWell shouldBeGreaterThan leftWell
+            middleWell shouldBeGreaterThanOrEqual (rightWell + leftWell)
+        }
+    })
