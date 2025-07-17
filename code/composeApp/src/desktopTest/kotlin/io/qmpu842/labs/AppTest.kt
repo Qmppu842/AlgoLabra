20bc56217fd2491c1cabc51746dcd72fdd40d4c7
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-07-17 12:28:16 +0300

    What a pointless time sink. Well now i know the datatest works, at least

--- code/composeApp/src/desktopTest/kotlin/io/qmpu842/labs/AppTest.kt
+++ code/composeApp/src/desktopTest/kotlin/io/qmpu842/labs/AppTest.kt
@@ -1,6 +1,7 @@
 package io.qmpu842.labs
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
 class AppTest :
@@ -10,3 +11,21 @@ class AppTest :
             1 + 2 shouldBe 3
         }
     })
+
+
+fun isPythagTriple(a: Int, b: Int, c: Int): Boolean = a * a + b * b == c * c
+
+data class PythagTriple(val a: Int, val b: Int, val c: Int)
+
+class MyTests : FunSpec({
+    context("Pythag triples tests") {
+         withData(
+            PythagTriple(3, 4, 5),
+            PythagTriple(6, 8, 10),
+            PythagTriple(8, 15, 17),
+            PythagTriple(7, 24, 25)
+        ) { (a, b, c) ->
+            isPythagTriple(a, b, c) shouldBe true
+        }
+    }
+})
