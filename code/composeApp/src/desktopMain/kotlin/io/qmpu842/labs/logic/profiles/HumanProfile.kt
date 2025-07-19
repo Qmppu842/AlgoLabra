5ab048279ae2c1ecf478c6410c040b0fa1ac2945
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-07-19 23:29:58 +0300

    Made HumanProfile.kt to allow random moves if the human is too lazy to think.

--- code/composeApp/src/desktopMain/kotlin/io/qmpu842/labs/logic/profiles/HumanProfile.kt
+++ code/composeApp/src/desktopMain/kotlin/io/qmpu842/labs/logic/profiles/HumanProfile.kt
@@ -1,9 +1,16 @@
 package io.qmpu842.labs.logic.profiles
 
+import io.qmpu842.labs.helpers.MyRandom
 import io.qmpu842.labs.logic.Board
 
-class HumanProfile(override val id: Int) : OpponentProfile {
+class HumanProfile(override val id: Int = MyRandom.random.nextInt()) : OpponentProfile {
+    val rand = MyRandom.random
+
+
+    /**
+     * There is one waaaaaay more fun thing but nah, that is not for now.
+     */
     override fun nextMove(board: Board, forSide: Int): Int {
-        TODO("Not yet implemented")
+        return board.getLegalMoves().random(rand)
     }
 }
