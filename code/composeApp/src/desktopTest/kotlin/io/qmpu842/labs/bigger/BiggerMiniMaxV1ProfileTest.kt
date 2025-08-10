266ddd770d6149a3655580e22996c8e8a6b54054
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-08-11 02:42:25 +0300

    This is impressive, depth 19 in reasonable time. Magic of a/b pruning and possibilities dropping like flies.
    Just imagine how mean the AI is when it can go full 42 with all the bells and whistles.

--- code/composeApp/src/desktopTest/kotlin/io/qmpu842/labs/bigger/BiggerMiniMaxV1ProfileTest.kt
+++ code/composeApp/src/desktopTest/kotlin/io/qmpu842/labs/bigger/BiggerMiniMaxV1ProfileTest.kt
@@ -82,17 +82,19 @@ class BiggerMiniMaxV1ProfileTest :
         context("nextMove data tests") {
             withData(
                 GameToPlay(
+                    depth = 6,
+                    start = "4441534236441655572",
+                    end = "4441534236441655572535332",
+                ),
+                GameToPlay(
                     depth = 9,
                     start = "44444222245355266776662611135533",
                     end = "44444222245355266776662611135533353777711",
                 ),
                 GameToPlay(
+                    depth = 9,
                     start = "44444222273746666773171171556112",
                     end = "44444222273746666773171171556112555536233",
-                ),
-                GameToPlay(
-                    start = "4441534236441655572",
-                    end = "4441534236441655572535332",
                 ),
                 GameToPlay(
                     depth = 16,
@@ -104,35 +106,60 @@ class BiggerMiniMaxV1ProfileTest :
                     start = "4423432337225575224445",
                     end = "44234323372255752244455573377711111166",
                 ),
-//                GameToPlay(
-//                    playerA =
-//                        MiniMaxV1Profile(
-//                            depth = 40,
-//                            timeLimit = TRILLION,
-//                        ),
-//                    playerB =
-//                        MiniMaxV1Profile(
-//                            depth = 40,
-//                            timeLimit = TRILLION,
-//                        ),
-//                    start = "4",
-//                    end =   "44444666646555621237353353357112222711177",
-//                ),
-//                GameToPlay(
-//                    depth = 20,
-//                    start = "444441566664322362253",
-//                    end = "44444666646555621237353353357112222711177",
-//                ),
-
-//                GameToPlay(
-//                    depth = 18,
-//                    start = "444441566664322362253151",
-//                    end = "44444666646555621237353353357112222711177",
-//                ),
                 GameToPlay(
                     depth = 16,
                     start = "4444415666643223622531511",
                     end = "44444156666432236225315115525633327771177",
+                ),
+                GameToPlay(
+                    depth = 17,
+                    start = "4444415666643223622531555",
+                    end = "44444156666432236225315552563332777111177",
+                ),
+                GameToPlay(
+                    depth = 18,
+                    start = "444441566664322362253155",
+                    end = "44444156666432236225315552563332777111177",
+                ),
+            ) { (playerA, playerB, start, end) ->
+                val bc = BoardConfig()
+
+                var gameHolder =
+                    GameHolder(
+                        board = Board(bc, start, offset = -1),
+                        playerA = playerA,
+                        playerB = playerB,
+                        bc = bc,
+                    )
+
+                repeat(end.length - start.length) {
+                    gameHolder = gameHolder.dropTokenLimited()
+                }
+
+                gameHolder.whoisWinner() shouldBe if (end.length % 2 == 0) -1 else 1
+
+                val history =
+                    gameHolder.board.history
+                        .map { thing -> thing + 1 }
+                        .joinToString("")
+                history shouldBe end
+            }
+        }
+
+        context("nextMove data tests the long ones") {
+            withData(
+                /**
+                 * This can be run in reasonable time, WOW!
+                 */
+                GameToPlay(
+                    depth = 19,
+                    start = "44444156666432236225315",
+                    end = "44444156666432236225315552563332777111177",
+                ),
+                GameToPlay(
+                    depth = 20,
+                    start = "4444415666643223622531",
+                    end = "44444156666432236225315552563332777111177",
                 ),
             ) { (playerA, playerB, start, end) ->
                 val bc = BoardConfig()
